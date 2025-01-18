98d552573902d1c1b99be922a34b07d363ffd369
YCode <joestudy@petalmail.com>
2025-01-18 21:56:35 +0800

    Add project files.

---
+++ LostFocusDemo/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using ActiproSoftware.Windows.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LostFocusDemo
+{
+	internal class MainViewModel
+	{
+		public MainViewModel()
+		{
+			this.RootNode = new TreeNode() { Name = "Root" };
+
+			this.RootNode.Children.Add(new TreeNode()
+			{
+				Name = "Node1"
+			});
+
+			this.RootNode.Children.Add(new TreeNode()
+			{
+				Name = "Node2"
+			});
+
+			this.DelCommand = new DelegateCommand<TreeNode>((parameter) =>
+			{
+				this.RootNode.Children.Remove(parameter);
+			});
+		}
+
+		public TreeNode RootNode { get; set; }
+
+		public ICommand DelCommand { get; set; }
+	}
+}
---
+++ LostFocusDemo/TreeNodeItemAdapter.cs
@@ -0,0 +1,18 @@
+﻿using ActiproSoftware.Windows.Controls.Grids;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostFocusDemo
+{
+	internal class TreeNodeItemAdapter : TreeListBoxItemAdapter
+	{
+		public override IEnumerable GetChildren(TreeListBox ownerControl, object item)
+		{
+			return item is TreeNode node ? node.Children : base.GetChildren(ownerControl, item);
+		}
+	}
+}
